443ca77217144739bd7bc6619cc80b5ee0716d15
ihgs <hgs.dev@gmail.com>
2019-10-03 22:49:31 +0900

    enable to diff

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div v-if="showdiff">
+      <vue-code-diff :old-string="oldStr" :new-string="newStr" :context="10" outputFormat="side-by-side"/>
+    </div>
   </div>
 </template>
+ 
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import vueCodeDiff from 'vue-code-diff'
+import axiosBase from 'axios'
 
+const axios = axiosBase.create({ headers:{'Content-Type': 'text/plain'}, transformResponse: (res) => {
+            
+            return res;
+        }})
+const diff_url = () => {
+  const value = window.location.href.match(new RegExp("[?&]diff=(.*?)\\.{3}(.*?)(&|$|#)"));
+  if (value == null) return '';
+  return [decodeURIComponent(value[1]), decodeURIComponent(value[2])];
+}
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  components: {vueCodeDiff},
+  data(){
+    return {
+      oldStr: null,
+      newStr: null
+    }
+  },
+  created: function(){
+    const urls = diff_url()
+    const base = 'https://gist.githubusercontent.com/'
+    axios.get(base + urls[0]).then( (response) => {
+      this.oldStr = response.data
+    })
+    axios.get(base + urls[1]).then( (response) =>{
+      this.newStr = response.data
+    })
+  },
+  computed: {
+    showdiff: function(){
+      return this.oldStr && this.newStr 
+    }
   }
 }
+
+
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
